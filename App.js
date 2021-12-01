e18b26ab374b974af3ecbf4f9d9b2541e0e60e94
JongCHONG <yonhoa27@hotmail.fr>
2021-12-01 14:57:03 +0100

    2 - ScrollView

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, ScrollView, Image, Pressable, Alert } from 'react-native';
 
 export default function App() {
+  const createTwoButtonAlert = () => {
+    Alert.alert(
+      "Alert Title",
+      "My Alert Msg",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]
+    )
+  }
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={{backgroundColor: "white"}}>
         <Text style={{fontSize: 30}}>Konexio!</Text>
         <Text style={{textAlign: "center"}}>Konexio!</Text>
         <Text style={{fontWeight: "bold"}}>Konexio!</Text>
+        <Image style={styles.img} source={{url:'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png'}} />
+        <Image style={styles.img} source={require('./assets/konexio-logo_1.png')} />
+        <Button title={"2-Button Alert"} onPress={createTwoButtonAlert} />
+        <Pressable onPress={createTwoButtonAlert}>
+          <Text>
+            test
+          </Text>
+        </Pressable>
       </View>
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -20,4 +43,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'blue',
     padding: 10,
   },
+  img: {
+    height: 100,
+  }
 });
